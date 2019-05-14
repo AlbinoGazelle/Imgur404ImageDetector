9cee87a26f8095894d40bf0b584614079832597c
AlbinoGazelle <37495851+AlbinoGazelle@users.noreply.github.com>
2019-05-13 23:04:31 -0700

    Add initial file (Just lists files and hashes)

---
+++ imageHash.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import imagehash
+import os
+import json
+#first get filenames in directory
+#then calculate the hash for each file
+#compare bad hash to list of all hashes
+#delete any files that has bad hash
+def find_images(userpaths, hashfunc = imagehash.average_hash):
+    def is_image(filename):
+        f = filename.lower()
+        return f.endswith(".png") or f.endswith(".jpg") or \
+                f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif") or '.jpg' in f
+
+    image_filenames = []
+    for userpath in userpaths:
+        image_filenames += [os.path.join(userpath, path) for path in os.listdir(userpath) if is_image(path)]
+    images = {}
+    for img in sorted(image_filenames):
+        try:
+            hash = hashfunc(Image.open(img))
+            print('Image:', img, 'Hash:', hash)
+        except Exception as e:
+            print('Problem:', e, 'with', img)
+        if hash in images:
+            print(img, ' already exists')
+
+path = ['C:\\Test']
+find_images(path)
