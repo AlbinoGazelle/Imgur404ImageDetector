5cebcc85866fc9295acd7df574f1a8217813480f
AlbinoGazelle <37495851+AlbinoGazelle@users.noreply.github.com>
2019-05-14 16:13:09 -0700

    Add recursion and detecting corrupt files
    still need to add threading and deleting corrupt files

--- imageHash.py
+++ imageHash.py
@@ -2,28 +2,35 @@ from PIL import Image
 import imagehash
 import os
 import json
-#hash of imgur404 images
+import sys
+import hashlib
+from threading import Thread, Lock
+#imgur404 image hash
 badHash = 'c0fcfffefe00e040'
+#define walk_dir as argument user gave when running script
+walk_dir = sys.argv[1]
 def find_images(userpaths, hashfunc = imagehash.average_hash):
+    #only get hash of images
     def is_image(filename):
         f = filename.lower()
         return f.endswith(".png") or f.endswith(".jpg") or \
                 f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif") or '.jpg' in f
+    for root, subdirs, files in os.walk(walk_dir):
+        for filename in files:
+            result = is_image(filename)
+            #if file is an image
+            if result:
+                try:
+                    #define file_path
+                    file_path = os.path.join(root, filename)
+                    #get hash of file and convert to str
+                    hash = str(hashfunc(Image.open(file_path)))
+                    if hash in badHash:
+                        print('Deleting Imgur404 Image!', file_path)
+                        #delete file
+                        os.remove(file_path)
+                #for corrupted files(maybe in future delete those file? still need to do more testing)
+                except Exception as e:
+                    print('Problem', e, 'with', file_path, 'Maybe Corrupt File?')
 
-    image_filenames = []
-    for userpath in userpaths:
-        image_filenames += [os.path.join(userpath, path) for path in os.listdir(userpath) if is_image(path)]
-    for img in sorted(image_filenames):
-        try:
-            hash = hashfunc(Image.open(img))
-            hash = str(hash)
-        except Exception as e:
-            print('Problem:', e, 'with', img)
-        if hash in badHash:
-            print('Deleting:', img, 'Because it has bad hash!')
-            os.remove(img)
-
-#for testing
-#still need to add config file support
-path = ['C:\\Test']
-find_images(path)
+find_images(walk_dir)
